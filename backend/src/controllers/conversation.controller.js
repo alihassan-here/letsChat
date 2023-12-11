ade886b188f59aef736893244ee5f3aadf86044e
Ali Hassan <alihassanuog50@gmail.com>
2023-12-11 15:32:31 -0500

    create conversation end point

---
+++ backend/src/controllers/conversation.controller.js
@@ -0,0 +1,59 @@
+import createHttpError from "http-errors";
+import logger from "../configs/logger.config.js";
+import {
+    createConversation,
+    doesConversationExist,
+    populateConversation
+} from "../services/conversation.service.js";
+
+export const create_open_conversation = async (req, res, next) => {
+    try {
+        const sender_id = req.user.userId;
+        const { receiver_id, isGroup } = req.body;
+        if (isGroup == false) {
+            //check if receiver_id is provided
+            if (!receiver_id) {
+                logger.error(
+                    "please provide the user id you wanna start a conversation with !"
+                );
+                throw createHttpError.BadGateway("Oops...Something went wrong !");
+            }
+            //check if chat exists
+            const existed_conversation = await doesConversationExist(
+                sender_id,
+                receiver_id,
+                false
+            );
+            if (existed_conversation) {
+                res.json(existed_conversation);
+            } else {
+                // let receiver_user = await findUser(receiver_id);
+                let convoData = {
+                    name: "conversation name",
+                    picture: "conversation picture",
+                    isGroup: false,
+                    users: [sender_id, receiver_id],
+                };
+                const newConvo = await createConversation(convoData);
+                const populatedConvo = await populateConversation(
+                    newConvo._id,
+                    "users",
+                    "-password"
+                );
+                res.status(200).json(populatedConvo);
+            }
+        } else {
+            console.log("hnaaaaaaaaaa");
+            //it's a group chat
+            //check if group chat exists
+            const existed_group_conversation = await doesConversationExist(
+                "",
+                "",
+                isGroup
+            );
+            res.status(200).json(existed_group_conversation);
+        }
+    } catch (error) {
+        next(error);
+    }
+}
